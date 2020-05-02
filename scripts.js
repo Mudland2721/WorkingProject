9517f07c39bacae41c7594021f13702e544f6aac
Mason <Mudland2721@gamil.com>
2020-05-02 11:01:17 -0600

    added diff boxes

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// $( document ).ready(function() {
+
+
+
+
+
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//       }).then(function(response) {
+//         console.log(response);
+//       });
+
+
+
+// });
