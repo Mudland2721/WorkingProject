9985be2d984ecfd91c58594d1a8b53a5d2a9cbcc
Mudland2721 <Mudland2721@gmail.com>
2020-05-02 14:30:24 -0600

    fixed button

--- scripts.js
+++ scripts.js
@@ -1,23 +1,13 @@
-$('button').click(function () {
-    location.href = "https://www.lyft.com/rider"
+$(document).ready(function () {
+  $("#lyftButton").click(function () {
+    window.open("https://www.lyft.com/rider");
+    console.log("clicked");
+  });
 
-})
-
-
-// $( document ).ready(function() {
-
-
-
-
-
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//       }).then(function(response) {
-//         console.log(response);
-//       });
-
-
-
-// });
+  // $.ajax({
+  //     url: queryURL,
+  //     method: "GET"
+  //   }).then(function(response) {
+  //     console.log(response);
+  //   });
+});
